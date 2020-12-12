f2028398b74341ab1a203685e2f98cdda8ec8e5e
Pablo Bravo <pablo.bravo.bravo@udla.edu.ec>
2020-12-12 10:54:10 -0500

    commit

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,21 @@
 import React from 'react';
+
+
 const Login = (porps) => {
 
     const {email,setEmail,password,setPassword,handleLogin,handleSignup,hasAccount,sethasAccount,emailError,passwordError } = porps;
     return(
         <div className="flex justify-center mt-10">
-            <div className="w-full max-w-3xl">
+            <div className="w-full max-w-3xl">      
+                <img id="Logo" src={"https://todoaki.com/wp-content/uploads/1520627_1571992646348451_6162017968943298432_n46.jpg"} alt=" imagen logo" />
+                <div>           
+                <h1 id="Titulo" className="block text-orange-700 text-m font-bold mb-2"> 
+                    Campiña Lojana 
+                    </h1>
+                </div>
                 <section className="login text-3xl font-light mb-4" >
                     <div className='LoginContainer shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'>
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Usuario</label>
+                        <label className="block text-red-700 text-sm font-bold mb-2">Usuario</label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="text" 
@@ -18,7 +26,7 @@ const Login = (porps) => {
                         />
                         
                         <p className="errorMsg font-bold">{emailError}</p>
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Contraseña</label>
+                        <label className="block text-red-700 text-sm font-bold mb-2">Contraseña</label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="password"  
@@ -30,17 +38,16 @@ const Login = (porps) => {
                         <div className="btnContainer">
                         {hasAccount ? (
                             <>
-                            <buttom onClick={handleLogin} >Entrar</buttom>
-                            <p className="font-bold"> 
-                            ¿No tienes cuenta aun? {""}
-                            </p> <span onClick={() => sethasAccount(!hasAccount)}>Registrate</span>
+                                <buttom onClick={handleSignup} >Registrate</buttom>
+                                <p className="font-bold">
+                                    ¿Ya tienes una cuenta?{""} 
+                                </p> <buttom onClick={() => sethasAccount(!hasAccount)} >Entrar</buttom>
                             </>
                             ) : (
                             <>
-                            <buttom onClick={handleSignup} >Registrate</buttom>
-                            <p className="font-bold">
-                                ¿Ya tienes una cuenta?{""} 
-                            </p> <span onClick={() => sethasAccount(!hasAccount)} >Entrar</span>
+                                <p className="font-bold"></p>
+                                <button id="btnLogin" onClick={handleLogin} 
+                                className="block text-orange-700 text-m font-bold mb-1" >Entrar</button>
                             </>
                         )}
                         </div>
@@ -51,3 +58,4 @@ const Login = (porps) => {
     )
 };
 export default Login;
+
